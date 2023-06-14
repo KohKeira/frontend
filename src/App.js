545b86c64d07bae57947c10a128297d6a105ce06
Keira <kohkeira996@gmail.com>
2023-06-14 14:13:11 +0800

    test

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
               <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>
-                  Edit <code>src/App.tsx</code> and save to reload.
+                  Hello
                 </p>
               </header>
             </div>
--- src/screens/Todo.js
+++ src/screens/Todo.js
@@ -1,7 +1,39 @@
 import { Container, Button, Form, FormCheck } from "@govtechsg/sgds-react";
 import Table from "../components/Table";
+import { useState, useEffect } from 'react';
+import axios from "axios";
+import CONFIG from "../config";
+
+function TodoItem(props) {
+  const [done, setDone] = useState(props.done);
+  return (
+    <>
+      <tr>
+        <td>
+          <FormCheck
+            checked={done}
+          />
+        </td>
+        <td width={"100%"}>{props.description}</td>
+      </tr>
+    </>
+  );
+}
 
 function Todo() {
+  const [todoItems, setTodoItems] = useState({});
+
+  useEffect(() => {
+    populateTodos();
+}, [todoItems]);
+
+  const populateTodos = () => {
+
+  axios.get(`${CONFIG.API_ENDPOINT}`)
+      .then((result) => {
+      setTodoItems(result.data);
+      })
+  }
   const today = new Date();
   const dateOptions = {
     day: "numeric",
@@ -38,6 +70,17 @@ function Todo() {
               </td>
             </tr>
           </tbody>
+          {/* Iterate through todoItems list to create new rows */}
+{Object.keys(todoItems).map((item) => (
+    // Forwards items to TodoItem element as props
+    <TodoItem
+    key={todoItems[item].id}
+    id={todoItems[item].id}
+    done={todoItems[item].done}
+    description={todoItems[item].description}
+    refreshToDos={populateTodos}
+    />
+))}
         </Table>
       </Form>
       <Button size="sm" variant="primary">
